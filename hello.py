63da92ce518e492744e1717d13442425d4e7d9b2
gmalhotra <giovannamalhotra@Giovannas-MacBook-Pro.local>
2017-09-24 18:07:13 -0700

    last commit - modified last print

--- hello.py
+++ hello.py
@@ -1,3 +1,3 @@
 print("Hello dear Dr. Panda, thanks much for everything you do for me!!")
 print("This is so helpful and interesting!! :)")
-print("Another message to test multiple commits")
+print("Another message to test multiple commits - changes this message for last commit")
