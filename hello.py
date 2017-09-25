5d41a2ec20115180025ebac0b79b127d678aa397
gmalhotra <giovannamalhotra@Giovannas-MacBook-Pro.local>
2017-09-24 19:03:33 -0700

    Changes from my forked repo

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
 print("Hello dear Dr. Panda, thanks much for everything you do for me!!")
 print("This is so helpful and interesting!! :)")
 print("Another message to test multiple commits - changes this message for last commit")
+print("Adding a change from my forked repo")
